20b66366923dc7294ae5899b11005559f109361b
emtalen <59577366+emtalen@users.noreply.github.com>
2020-03-11 21:03:20 +0100

    modifying controller after example from code review comment

--- app/controllers/api/ingredients_controller.rb
+++ app/controllers/api/ingredients_controller.rb
@@ -1,27 +1,28 @@
 class Api::IngredientsController < ApplicationController
-  def index
-    ingredients = [
-      "Coca-Cola", 
-      "Lemon", 
-      "Milk", 
-      "Egg white", 
-      "Lime juice", 
-      "Sugar Syrup", 
-      "Whipping cream", 
-      "Orange juice", 
-      "Lemon juice", 
-      "Coffee", 
-      "Lime", 
-      "Lemon", 
-      "Orange", 
-      "Pineapple juice", 
-      "Tonic Water", 
-      "Mint", 
-      "Ginger", 
-      "Ice", 
-      "Ginger ale", 
-      "Passion fruit juice"
-    ]
-    render json: { ingredients: ingredients }
+  AVAILABLE_INGREDIENTS = [
+    "Coca-Cola", 
+    "Lemon", 
+    "Milk", 
+    "Egg white", 
+    "Lime juice", 
+    "Sugar Syrup", 
+    "Whipping cream", 
+    "Orange juice", 
+    "Lemon juice", 
+    "Coffee", 
+    "Lime", 
+    "Lemon", 
+    "Orange", 
+    "Pineapple juice", 
+    "Tonic Water", 
+    "Mint", 
+    "Ginger", 
+    "Ice", 
+    "Ginger ale", 
+    "Passion fruit juice"
+  ]
+  
+  def index  
+    render json: { ingredients: API::IngredientsController.AVAILABLE_INGREDIENTS }
   end
 end
