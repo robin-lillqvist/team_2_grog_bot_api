938b7050ec3cec69f04f5d0c6737fc91afdbbe1a
emtalen <59577366+emtalen@users.noreply.github.com>
2020-03-12 15:37:20 +0100

    fixes formatting in the user_can_see_ingredients_spec.rb

--- spec/requests/api/user_can_see_ingredients_spec.rb
+++ spec/requests/api/user_can_see_ingredients_spec.rb
@@ -1,16 +1,15 @@
 RSpec.describe Api::IngredientsController, type: :request do
-    
-    describe "GET /api/ingredients" do
-        before do
-            get "/api/ingredients"
-        end
-
-        it "should return a 200 response" do
-            expect(response).to have_http_status 200
-        end
-
-        it "should return ingredient" do
-            expect((response_json)["ingredients"].count).to eq 20
-        end
+  describe "GET /api/ingredients" do
+    before do
+      get "/api/ingredients"
     end
+
+    it "should return a 200 response" do
+    expect(response).to have_http_status 200
+    end
+
+    it "should return ingredient" do
+    expect((response_json)["ingredients"].count).to eq 20
+    end
+  end
 end
