8a14fe9fb6f4d0bf9051d7fe45029f84b2ffdf4e
emtalen <59577366+emtalen@users.noreply.github.com>
2020-03-12 16:00:47 +0100

    installing formatter and formats the code in the spec again2

--- spec/requests/api/user_can_see_ingredients_spec.rb
+++ spec/requests/api/user_can_see_ingredients_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe Api::IngredientsController, type: :request do
     end
 
     it "should return a 200 response" do
-    expect(response).to have_http_status 200
+      expect(response).to have_http_status 200
     end
 
     it "should return ingredient" do
-    expect((response_json)["ingredients"].count).to eq 20
+      expect((response_json)["ingredients"].count).to eq 20
     end
   end
 end
