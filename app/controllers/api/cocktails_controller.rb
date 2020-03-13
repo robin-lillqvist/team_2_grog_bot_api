f75115f99cc90620146d4ea1e9cc72b658f8ea01
bounce <johan@waytogo.se>
2020-03-13 14:45:33 +0100

    removed binding.pry, formatted code, and deleted empty show method.

--- app/controllers/api/cocktails_controller.rb
+++ app/controllers/api/cocktails_controller.rb
@@ -1,22 +1,18 @@
 class Api::CocktailsController < ApplicationController
   def index
     if AVAILABLE_INGREDIENTS.include? params[:q]
-    response = RestClient.get(
-      "https://www.thecocktaildb.com/api/json/v2/9973533/filter.php",
-      {
-        params: {
-          i: params[:q]
+      response = RestClient.get(
+        "https://www.thecocktaildb.com/api/json/v2/9973533/filter.php",
+        {
+          params: {
+            i: params[:q]
+          }
         }
-      }
-    )
-    results = JSON.parse(response)
-      render json: { drinks: results["drinks"] }
-      binding.pry
+      )
+      results = JSON.parse(response)
+        render json: { drinks: results["drinks"] }
     else
-    render json: { message: "You can not choose this ingredient" }, status: 422
+      render json: { message: "You can not choose this ingredient" }, status: 422
     end
-  end
-
-  def show
   end
 end
