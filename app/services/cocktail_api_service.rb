e940988f03471882d53b4d6b04e09f7c786f1f41
Thomas <thomas@craftacademy.se>
2020-03-15 21:26:51 +0100

    formats code in various files

--- app/services/cocktail_api_service.rb
+++ app/services/cocktail_api_service.rb
@@ -1,16 +1,18 @@
-module CocktailApiService
-	def self.get_by_ingredient(ingredient)
-		response = RestClient.get(
-			'https://www.thecocktaildb.com/api/json/v2/9973533/filter.php',
-			params: {
-				i: ingredient
-			}
-		)
-		results = JSON.parse(response)
-	end
+# frozen_string_literal: true
 
-	def self.get_by_id(id)
-		response = RestClient.get(
+module CocktailApiService
+  def self.get_by_ingredient(ingredient)
+    response = RestClient.get(
+      'https://www.thecocktaildb.com/api/json/v2/9973533/filter.php',
+      params: {
+        i: ingredient
+      }
+    )
+    results = JSON.parse(response)
+  end
+
+  def self.get_by_id(id)
+    response = RestClient.get(
       'https://www.thecocktaildb.com/api/json/v2/9973533/lookup.php',
       params: {
         i: id
@@ -18,5 +20,5 @@ module CocktailApiService
     )
     results = JSON.parse(response)
     cocktail = results['drinks'].first
-	end
+  end
 end
--- spec/requests/api/user_can_show_specific_cocktail_spec.rb
+++ spec/requests/api/user_can_show_specific_cocktail_spec.rb
@@ -1,29 +1,31 @@
+# frozen_string_literal: true
+
 RSpec.describe Api::CocktailsController, type: :request do
-  describe "GET /api/cocktails/12754 successfully" do
+  describe 'GET /api/cocktails/12754 successfully' do
     before do
-      get "/api/cocktails/12754"
+      get '/api/cocktails/12754'
     end
 
-		it "returns a 200 status" do
+    it 'returns a 200 status' do
       expect(response.status).to eq 200
     end
 
-		it "returns name of drink" do
-      expect(response_json["drink"]["name"]).to eq "Sex on the Beach"
+    it 'returns name of drink' do
+      expect(response_json['drink']['name']).to eq 'Sex on the Beach'
     end
   end
 
-  describe "GET /api/cocktails/ unsuccessfully" do
+  describe 'GET /api/cocktails/ unsuccessfully' do
     before do
-      get "/api/cocktails/99999"
+      get '/api/cocktails/99999'
     end
 
-    it "returns a 422 status" do
+    it 'returns a 422 status' do
       expect(response.status).to eq 422
     end
 
-    it "returns a message" do
-      expect(response_json["message"]).to eq "Oops, we could not find this cocktail"
-    end
+    it 'returns a message' do
+      expect(response_json['message']).to eq 'Oops, we could not find this cocktail'
+		end
   end
 end
